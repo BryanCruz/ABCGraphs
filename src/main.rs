0671b26d784298588c4227975ad1816788825fb3
BryanCruz <bryan.bialokur@gmail.com>
2024-11-25 19:57:36 -0300

    refacts

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,47 @@ mod benchmark;
 mod parsing;
 
 use benchmark::run_test;
-use parsing::{get_input_files_paths, get_output_file_path};
-use petgraph::algo::min_spanning_tree;
-use petgraph::data::FromElements;
-use petgraph::dot::{Config, Dot};
-use petgraph::graph::UnGraph;
 use serde_json;
 
 use std::{collections::HashMap, fs};
 
+// Config
+const N_BENCHMARK_FILES: u32 = 3;
+const N_TEST_EXECUTIONS: usize = 2;
+
 fn main() {
-    let graph = UnGraph::<i32, i32>::from_edges(&[
-        (1, 2, 10),
-        (2, 3, 11),
-        (3, 4, 10),
-        (2, 4, 13),
-        (1, 4, 15),
-    ]);
-
-    // Get the minimum spanning tree of the graph as a new graph, and check that
-    // one edge was trimmed.
-    let minimum_spanning_tree = UnGraph::<_, _>::from_elements(min_spanning_tree(&graph));
-    assert_eq!(
-        graph.raw_edges().len() - 2,
-        minimum_spanning_tree.raw_edges().len()
-    );
-
-    // Output the tree to `graphviz` `DOT` format
-    println!("{:?}", Dot::with_config(&graph, &[]));
-    println!("{:?}", Dot::with_config(&minimum_spanning_tree, &[]));
+    let single_target_output_file_path =
+        String::from("./output/single_target_petgraph_benchmarks.json");
+    let all_targets_output_file_path =
+        String::from("./output/all_targets_petgraph_benchmarks.json");
+    run_benchmark(true, single_target_output_file_path);
+    run_benchmark(false, all_targets_output_file_path);
 }
 
-fn benchmark_rust() {
-    let n_test_executions = 1;
+fn run_benchmark(single_target: bool, output_file_path: String) {
+    println!("Starting {}", output_file_path);
     let input_files_paths = get_input_files_paths();
 
-    let execution_times_in_ms: HashMap<&str, u128> = input_files_paths
+    let benchmarks: HashMap<&str, u128> = input_files_paths
         .iter()
         .map(|file_path| {
             (
                 get_file_name(file_path),
-                run_test(file_path, n_test_executions),
+                run_test(file_path, N_TEST_EXECUTIONS, single_target),
             )
         })
         .collect();
 
-    let output_file_path = get_output_file_path();
-    let output_file_content = serde_json::to_string(&execution_times_in_ms).unwrap();
+    let output_file_content = serde_json::to_string(&benchmarks).unwrap();
 
     fs::write(output_file_path, output_file_content)
         .expect("Should be able to write to output file.");
+}
+
+fn get_input_files_paths() -> Vec<String> {
+    (0..N_BENCHMARK_FILES)
+        .map(|index| format!("./input/benchmark_{}.in", index))
+        .collect()
 }
 
 fn get_file_name(file_path: &String) -> &str {
